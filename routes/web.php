a62d07973c11279948383388d2cf4ef41847574f
RafidAthallah132 <rafidathallah@gmail.com>
2021-11-18 15:10:39 +0700

    update_minor

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,8 @@ Route::post('hasil_perhitungan',"tugas_php@hasilperhitungan");
 Route::get('/pegawai','PegawaiController@index');
 Route::get('/pegawai/tambah','PegawaiController@tambah');
 Route::post('/pegawai/store','PegawaiController@store');
+
 Route::get('/pegawai/edit/{id}','PegawaiController@edit');
 Route::post('/pegawai/update','PegawaiController@update');
+
 Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
